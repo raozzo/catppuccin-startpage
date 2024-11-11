7a2ab1be5187f799663f38c82aa5ee500da6c3bf
raozzo <48601586+raozzo@users.noreply.github.com>
2024-11-11 21:50:46 +0100

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -16,7 +16,7 @@ const default_config = {
   openLastVisitedTab: true,
   tabs: [
     {
-      name: "personale",
+      name: "ME",
       background_url: "src/img/banners/cbg-9.gif",
       categories: [
         {
